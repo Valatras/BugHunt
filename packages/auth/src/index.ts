7dec16a67d702c08d7b967ccba984866d5b05930
Valatras <Arifcan091@gmail.com>
2026-03-30 13:31:39 +0200

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,32 @@
+import { expo } from "@better-auth/expo";
+import { createPrismaClient } from "@my-better-t-app/db";
+import { env } from "@my-better-t-app/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+export function createAuth() {
+  const prisma = createPrismaClient();
+
+  return betterAuth({
+    database: prismaAdapter(prisma, {
+      provider: "postgresql",
+    }),
+
+    trustedOrigins: [
+      env.CORS_ORIGIN,
+      "my-better-t-app://",
+      ...(env.NODE_ENV === "development"
+        ? ["exp://", "exp://**", "exp://192.168.*.*:*/**", "http://localhost:8081"]
+        : []),
+    ],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    plugins: [tanstackStartCookies(), expo()],
+  });
+}
+
+export const auth = createAuth();
